28597806556cf0251008e361b0b5e5d48e7c2e9b
Md Shabbir Jamal <mdshabbir820141@gmail.com>
2026-08-28 02:05:38 +0530

    codecrafters submit [skip ci]

---
+++ app/Enums.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class Type(Enum):
+    BUILTIN = 1
+    UNKNOWN = 2
--- app/builtins.py
+++ app/builtins.py
@@ -1,10 +1,14 @@
+from .Enums import Type
+
 def builtin_factory(command: str):
-    command_type = command.split(" ")[0]
+    command_type = command.split(maxsplit=1)[0]
     match command_type:
         case "exit":
             return ExitBuiltinCommand(command)
         case "echo":
             return EchoBuiltinCommand(command)
+        case "type":
+            return TypeBuiltinCommand(command)
         case _:
             return None
 
@@ -12,7 +16,8 @@ def builtin_factory(command: str):
 class BuiltinCommand:
 
     def __init__(self, command_str: str):
-        self.command = command_str
+        self.command_param = " ".join(command_str.split(" ")[1:])
+        self.type = Type.BUILTIN
 
     def operation(self):
         pass
@@ -24,6 +29,16 @@ class ExitBuiltinCommand(BuiltinCommand):
 
 class EchoBuiltinCommand(BuiltinCommand):
     def operation(self):
-        print_words = self.command.split(" ")[1:]
+        print_words = self.command_param
         print(" ".join(print_words))
+        return False, None
+
+class TypeBuiltinCommand(BuiltinCommand):
+    def operation(self):
+        command = builtin_factory(self.command_param)
+        if command is None:
+            print(f"{self.command_param.split(maxsplit=1)[0]}: not found")
+        elif command.type == Type.BUILTIN:
+            print(f"{self.command_param.split(maxsplit=1)[0]} is a shell builtin")
+
         return False, None
